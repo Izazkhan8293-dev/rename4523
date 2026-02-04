237faf7fb0e2a64651da8a35a8843d8db87e524a
Izazsarbudeen <izazkhanimran005@gmail.com>
2026-02-04 15:02:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")        # required now
+API_ID = int(os.getenv("API_ID", "7839182"))
+API_HASH = os.getenv("API_HASH", "c4787b79c1bc855dbcb6d7f84be81883")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7371314077:AAGMWMl3C8aHoXxqQ-BpHoX7ojbX4ISqEnc")        # required now
 STRING_SESSION = os.getenv("STRING_SESSION")  # optional, for 4GB uploads
-ADMIN = int(os.getenv("ADMIN", 0))
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
-FORCE_SUBS = os.getenv("FORCE_SUBS", "")
+ADMIN = int(os.getenv("ADMIN", 1430259940))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1001685382274))
+FORCE_SUBS = os.getenv("FORCE_SUBS", "-1001685382274")
 START_PIC = os.getenv("START_PIC", "")
