f05478945a3f910b3e8c150f242c8bd55e74091b
Izazsarbudeen <izazkhanimran005@gmail.com>
2026-02-04 12:02:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,14 @@ import os
 import asyncio
 import threading
 from flask import Flask
-from pyrogram import idle
-from plugins.cb_data import app  # session-based bot client
+from plugins.cb_data import app
 
 # ------------------ Flask Web Server ------------------
 web_app = Flask(__name__)
 
 @web_app.route('/')
 def home():
-    return "✅ Rename-Bot-4GB is running! - @JishuBotz"
+    return "✅ Rename-Bot is running! "
 
 def run_web():
     web_app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
@@ -19,11 +18,12 @@ def run_web():
 async def run_bot():
     await app.start()
     print("✅ Bot started successfully")
+    await asyncio.sleep(1)
+    from pyrogram import idle
     await idle()
     await app.stop()
 
 # ------------------ Entry Point ------------------
 if __name__ == "__main__":
-    web_thread = threading.Thread(target=run_web)
-    web_thread.start()
+    threading.Thread(target=run_web).start()
     asyncio.run(run_bot())
